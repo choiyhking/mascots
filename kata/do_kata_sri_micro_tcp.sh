a8a85c76ec80e9b29f644a389e2732417ddced15
Yunha Choi <choiyhking@knu.ac.kr>
2025-04-07 22:55:04 +0900

    fix

--- kata/do_kata_sri_micro_tcp.sh
+++ kata/do_kata_sri_micro_tcp.sh
@@ -1,39 +1,67 @@
 #!/bin/bash
 
+
 KATA_CONFIG="/opt/kata/share/defaults/kata-containers/configuration.toml"
 
-sudo sed -i "s/^default_vcpus = [0-9]\+/default_vcpus = 2/" "${KATA_CONFIG}"
+sudo sed -i "s/^default_vcpus = [0-9]\+/default_vcpus = 3/" "${KATA_CONFIG}"
 sudo sed -i "s/^default_memory = [0-9]\+/default_memory = 4096/" "${KATA_CONFIG}"
+
 
 CONTAINER_NAME=sri-micro-tcp-kata
 IMAGE_NAME=sri-micro-tcp
+SERVER_IP="192.168.51.202"
 
-# 이런거 common.h 파일로 통일 
-if nc -z -w2 192.168.51.201 12865; then
-    echo "✅ netserver is reachable at 192.168.51.201:12865"
-else
-    echo "❌ netserver not reachable."
-    echo "[Run] sudo docker run -d --name sri-micro-tcp-netserver -p 12865:12865 -p 5001:5001 choiyhking/sri-micro-tcp netserver"
-    exit 1
-fi
 
-sudo docker run -d -q --name ${CONTAINER_NAME} \
-		--runtime=io.containerd.kata.v2 \
-		${IMAGE_NAME}
-
-	
 if [ "$#" -eq 0 ]; then
     echo "Usage: $0 [stream|rr]"
     exit 1
 fi
 
+if nc -z -w2 "$SERVER_IP" 12865; then
+    echo "netserver is reachable at "$SERVER_IP":12865"
+else
+    echo "netserver not reachable."
+    echo "[Run] sudo docker run -d --name sri-micro-tcp-netserver -p 12865:12865 -p 5001:5001 choiyhking/sri-micro-tcp"
+    echo "[Run] sudo docker start sri-micro-tcp-netserver"
+    echo "[Run] sudo docker exec sri-micro-tcp-netserver netserver"
+    exit 1
+fi
+
+
+if [ "$(sudo docker ps -a -q -f name="^${CONTAINER_NAME}$")" ]; then
+    STATUS=$(sudo docker inspect -f '{{.State.Status}}' "$CONTAINER_NAME")
+    if [ "$STATUS" = "exited" ] || [ "$STATUS" = "created" ]; then
+        echo "Container exists but is not running. Starting it..."
+        sudo docker start "$CONTAINER_NAME"
+    else
+        echo "Container '$CONTAINER_NAME' is already running."
+    fi
+else
+    sudo docker run -d -q --name ${CONTAINER_NAME} \
+	    --runtime=io.containerd.kata.v2 \
+	    ${IMAGE_NAME}:latest
+fi
+
+
+sleep 5
+	
+
+
 if [ "$1" == "stream" ]; then
-    sudo docker exec "$CONTAINER_NAME" ./do_tcp_stream.sh
+    sudo docker exec "$CONTAINER_NAME" ./in_do_tcp_stream.sh
 elif [ "$1" == "rr" ]; then
-    sudo docker exec "$CONTAINER_NAME" ./do_tcp_rr.sh
+    sudo docker exec "$CONTAINER_NAME" ./in_do_tcp_rr.sh
 else
     echo "Wrong option: $1"
     exit 1
 fi
 
-sudo docker cp "$CONTAINER_NAME":/result_tcp_$1 ./kata_result_tcp_$1
+
+OUTPUT_DIR="result_tcp_$1"
+if [ -d "$OUTPUT_DIR" ]; then
+    i=1; while [ -d "${OUTPUT_DIR}.$i" ]; do ((i++)); done
+    mv "$OUTPUT_DIR" "${OUTPUT_DIR}.$i"
+fi
+
+sudo docker cp "$CONTAINER_NAME":/$OUTPUT_DIR ./$OUTPUT_DIR
+sudo docker stop "$CONTAINER_NAME"
